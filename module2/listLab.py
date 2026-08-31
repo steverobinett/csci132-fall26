31f9650db8bea5e7fddc73aacd1d9d8028a826d6
Steve Robinett <steven.robinett@gfcmsu.edu>
2026-08-31 11:30:28 -0600

    aug 31 lab

---
+++ module2/listLab.py
@@ -0,0 +1,32 @@
+def main():
+    groceries = ['Apples', 'Oranges', 'Bananas','Grapes', 'Lemons']
+   
+    myList = []
+    myList.append('Chips')
+
+    print(groceries[0:])
+
+    newG = groceries[1:3]
+    print(newG)
+
+    #adding
+    groceries.append('Chips')
+    groceries.insert(3,'cookies')
+    print(groceries)
+
+    x = groceries.pop()
+    print(x)
+    print(groceries)
+
+    #mutable
+    groceries[2] = 'ZZZZZZ'
+
+    print(groceries)
+
+    finalGroc = tuple(groceries)
+    print(finalGroc)
+    finalGroc[1] = 'zzzzz'
+    print(finalGroc)
+
+    
+main()
